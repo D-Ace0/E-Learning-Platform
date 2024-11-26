facadd1c8a81bf1f7a6ce20b85cee3631a0015c7
3abz75 <abdelrahman.ahmed75a@mail.com>
2024-11-26 13:39:43 +0200

    quiz  5 crud tested

--- src/quiz/quiz.service.ts
+++ src/quiz/quiz.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
-import { Quiz } from 'src/quiz/models/quiz.schema'
+import { Quiz, QuizDocument } from 'src/quiz/models/quiz.schema'
 import { CreateQuizDto } from 'src/quiz/dto/create.quiz.dto'
 import { UpdateQuizDto } from 'src/quiz/dto/update.quiz.dto'
 import mongoose from 'mongoose'
@@ -14,29 +14,29 @@ export class QuizService {
   ) {}
 
   // create a quiz
-  async create(quizData: CreateQuizDto): Promise<Quiz> {
+  async create(quizData: CreateQuizDto): Promise<QuizDocument> {
     const newQuiz = new this.quizModel(quizData) // Use DTO for quiz creation
     return await newQuiz.save() // Save it to the database
   }
 
   // Get all quizzes
-  async findAll(): Promise<Quiz[]> {
+  async findAll(): Promise<QuizDocument[]> {
     let quizzes = await this.quizModel.find()  // Fetch all quizzes from the database
     return quizzes
   }
 
   // Get a quiz by ID
-  async findById(quiz_id: mongoose.Types.ObjectId): Promise<Quiz> {
+  async findById(quiz_id: mongoose.Types.ObjectId): Promise<QuizDocument> {
     return await this.quizModel.findById(quiz_id)  // Fetch a quiz by ID
   }
 
   // Update a quiz's details by ID
-  async update(quiz_id: mongoose.Types.ObjectId, updateData: UpdateQuizDto): Promise<Quiz> {
+  async update(quiz_id: mongoose.Types.ObjectId, updateData: UpdateQuizDto): Promise<QuizDocument> {
     return await this.quizModel.findByIdAndUpdate(quiz_id, updateData, { new: true })  // Find and update the quiz
   } 
 
   // Delete a quiz by ID
-  async delete(quiz_id: mongoose.Types.ObjectId): Promise<Quiz> {
+  async delete(quiz_id: mongoose.Types.ObjectId): Promise<QuizDocument> {
     return await this.quizModel.findByIdAndDelete(quiz_id)  // Find and delete the quiz
   }
 
