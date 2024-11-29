3351c2ee195c3df8e8e86a731c2cf98b33e21dcd
Oppman97 <omarbadrawy77@gmail.com>
2024-11-29 18:38:09 +0200

    nothing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,29 @@
-import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { NotesModule } from './notes/notes.module';
-import * as process from 'node:process';
-import * as dotenv from 'dotenv';
-import { APP_GUARD } from '@nestjs/core';
-import { JwtService } from '@nestjs/jwt';
-import { LoggingMiddleware } from './middleware/loggerMiddleware';
-import { AuthorizationGuard } from './guards/authorization.guard';
-import { CourseModule } from './Courses/courses.module';
-import { QuizModule } from './quizzes/quiz.module';
-import { ModuleModule } from './modules/module.module';
-import { MfaModule } from './mfa/mfa.module';
-import { MailModule } from './mail/mail.module';
+import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
+import { AuthModule } from './auth/auth.module'
+import { UsersModule } from './users/users.module'
+import { MongooseModule } from '@nestjs/mongoose'
+import { NotesModule } from './notes/notes.module'
+import * as process from 'node:process'
+import * as dotenv from 'dotenv'
+import { APP_GUARD } from '@nestjs/core'
+import { JwtService } from '@nestjs/jwt'
+import { LoggingMiddleware } from './middleware/loggerMiddleware'
+import { AuthorizationGuard } from './guards/authorization.guard'
+import { CourseModule } from './Courses/courses.module'
+import { QuizModule } from './quizzes/quiz.module'
+import { ModuleModule } from './modules/module.module'
+import { MfaModule } from './mfa/mfa.module'
+import { MailModule } from './mail/mail.module'
 dotenv.config();
+
 
 @Module({
   providers: [
     {
       provide: APP_GUARD,
-      useClass: JwtService,
+      useClass: JwtService
     },
-    Logger,
+    Logger
   ],
   imports: [
     MailModule,
@@ -32,12 +33,13 @@ dotenv.config();
     UsersModule,
     NotesModule,
     ModuleModule,
-    MfaModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/E-Learning-Platform', {}), 
+    MfaModule, 
+     MongooseModule.forRoot(
+      'mongodb+srv://abdelrahmanahmed75a:PO0kY6HyPet6zamr@e-learning.sdk3y.mongodb.net/', {}),
   ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggingMiddleware).forRoutes('*');
+    consumer.apply(LoggingMiddleware).forRoutes('*')
   }
 }
