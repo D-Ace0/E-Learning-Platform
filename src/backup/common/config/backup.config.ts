584f7c93a854af493f318000ec01e62bfc2fa46b
Oppman97 <omarbadrawy77@gmail.com>
2024-11-30 02:34:44 +0200

    data backup

--- src/backup/common/config/backup.config.ts
+++ src/backup/common/config/backup.config.ts
@@ -1,5 +1,5 @@
 const config = {
-  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/elearning',  // MongoDB URI
+  mongoUri: process.env.MONGO_URI || 'mongodb+srv://Omar:Omar1234@omarbadrawy1.ceagv.mongodb.net/',  // MongoDB URI
   backupPath: process.env.BACKUP_PATH || './src/backup/backups',  
 };
 
