6bec96c5e984cb99ed3501a2d26cfaa06145180d
Abdullah Mohamed <128763899+Sicariusa@users.noreply.github.com>
2024-12-12 18:26:40 +0200

    changing path for frontend

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
-
+// haa
 const Signup = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
