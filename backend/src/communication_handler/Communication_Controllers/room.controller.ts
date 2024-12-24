b6115fb5d9b861e903da03052ca71b72dc8e58d3
Abdullah Mohamed <128763899+Sicariusa@users.noreply.github.com>
2024-12-25 01:29:23 +0200

    kolo done b communication

--- backend/src/communication_handler/Communication_Controllers/room.controller.ts
+++ backend/src/communication_handler/Communication_Controllers/room.controller.ts
@@ -1,30 +1,13 @@
-import { Controller, Post, Body, UseGuards, Param, Get } from '@nestjs/common';
-import { AuthorizationGuard } from '../../guards/authorization.guard'; // Ensure only admins access this
-import { AuthenticationGuard } from '../../guards/authentication.guard'; // Ensure only admins access this
-import { CreateRoomDto } from '../dto/create-room.dto';
-import { AddStudentDto } from '../dto/add-student.dto';
+import { Controller, Get } from '@nestjs/common';
 import { RoomService } from '../Communication_Service/room.service';
-import { Room } from '../Communication_schemas/room.schema';
+import { RoomDocument } from '../Communication_schemas/room.schema';
 
-@Controller('rooms')
-@UseGuards(AuthenticationGuard, AuthorizationGuard)
+@Controller('rooms') // Route is '/rooms'
 export class RoomController {
   constructor(private readonly roomService: RoomService) {}
 
   @Get()
-  async getAllRooms(): Promise<Room[]> {
+  async getAllRooms(): Promise<RoomDocument[]> {
     return this.roomService.getAllRooms();
-  }
-
-  @Post('create')
-  async createRoom(@Body() createRoomDto: CreateRoomDto) {
-    const { name, createdBy } = createRoomDto;
-    return this.roomService.createRoom(name, createdBy);
-  }
-
-  @Post('add-student')
-  async addStudentToRoom(@Body() addStudentDto: AddStudentDto) {
-    const { roomName, studentId } = addStudentDto;
-    return this.roomService.addStudentToRoom(roomName, studentId);
   }
 }
